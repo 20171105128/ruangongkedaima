49f8642b6bbc189b66f697aad3964f629232524b
20171105128 <s20171105128@admindeimac-88.local>
2018-06-27 10:27:27 +0800

    Initial Commit

---
+++ ruangongkedaima/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ruangongkedaima
+//
+//  Created by TAO on 18/6/27.
+//  Copyright © 2018年 TAO. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
